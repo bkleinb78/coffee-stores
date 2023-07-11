4687e81712b1f421fd4774b68ea82d42feb6a047
bkleinb78 <y2bk728@aol.com>
2023-07-11 17:15:19 -0500

    created coffee-store page + added some function + background pictures and fixed the coloring

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Banner from "../components/banner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log("hi banner button");
+  };
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Coffee Connoisseur</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>  
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
+        <Image src="/static/hero-image.png" width={700} height={400} />
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
