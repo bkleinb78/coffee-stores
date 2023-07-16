ba5c116c2465d29db1d287f17d46106dae96e113
bkleinb78 <y2bk728@aol.com>
2023-07-16 11:02:54 -0500

    added the card compnent

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Banner from "../components/banner";
+import Card from "../components/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,11 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+        <Card
+          name="DarkHorse Coffee"
+          imgUrl="/hero-image.png"
+          href="/coffee-store/darkhorse"
+        />
       </main>
     </div>
   );
