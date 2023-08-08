0c095df53692a64abe9a1d0ad6c14b773e73a24d
bkleinb78 <y2bk728@aol.com>
2023-08-08 00:36:15 -0500

    fixed a few issues in the card.js file and added some syntax to the index.js file

--- components/card.js
+++ components/card.js
@@ -7,8 +7,8 @@ import styles from "./card.module.css";
 const Card = (props) => {
   return (
     <Link href={props.href} className={styles.cardLink}>
-      <div className={styles.container}>
-        <div className={cls("glass", styles.cardHeaderWrapper)}>
+      <div className={cls("glass", styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
           <h2 className={styles.cardHeader}>{props.name}</h2>
         </div>
         <div className={styles.cardImageWrapper}>
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner";
 import Card from "../components/card";
 
+import coffeeStores from "../data/coffee-stores.json";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -27,12 +29,15 @@ export default function Home() {
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
         <div className={styles.cardLayout}>
-          <Card
-            name="DarkHorse Coffee"
-            imgUrl="/static/hero-image.png"
-            href="/coffee-store/darkhorse"
-            className={styles.card}
-          />
+          {coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
